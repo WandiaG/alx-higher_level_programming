544966291118d2f03980eccd6ffafdf449823c93
WandiaG <wandiagithinji@gmail.com>
2023-10-03 06:51:51 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Return a+b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    """Creates copy of a sring without the char at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:]
