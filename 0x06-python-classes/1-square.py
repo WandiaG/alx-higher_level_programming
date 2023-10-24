4ce18640696a34b42aac65a8f52d9aa99837ad4f
WandiaG <wandiagithinji@gmail.com>
2023-10-24 06:51:08 -0700

    classes and instances

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Aclass that defines the size of a square """
+
+class Square:
+    def __init__(self, size):
+
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+class Square:
+    """
+    Class that defines properties of square.
+    """
+    def __init__(self, size=0):
+        """Creates new instances of square.
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of square.
+
+        Returns: The area.
+        """
+        return self.__size ** 2
