0e2a4de1dae579ca5791fda7d95998197f935d5a
WandiaG <wandiagithinji@gmail.com>
2023-11-07 05:58:16 -0800

    update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
+"""JSON representation of an object (string)"""
+
 import json
 
+
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object(string).
-
-    Args:
-        my_obj (type): object to examine.
-
-    Returns:
-        str: JSON representation of object.
-    """
-    # print("type json.dumps(my_obj)--> {}".format(type(json.dumps(my_obj))))
-    # print("type my_obj--> {}".format(type(my_obj)))
-
-    # serializing json
+    """JSON representation the object (string)"""
     return json.dumps(my_obj)
