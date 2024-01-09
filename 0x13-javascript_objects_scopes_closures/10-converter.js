15a7b6aafda1380ece5f0d26c150365e895b38a4
WandiaG <wandiagithinji@gmail.com>
2024-01-09 05:09:58 -0800

    fixing

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-/**
- * function that converts a number from base 10 base passed as argument
- * @param {number} base - base to examine
- * @returns {any} - the number converted to given base
- */
+
 exports.converter = function (base) {
-  function converted (number) {
-    return number.toString(base);
-  }
-  return converted;
+  return function convert (num) {
+    return num.toString(base);
+  };
 };
+
