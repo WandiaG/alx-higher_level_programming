e2fd2ba78ada5958f59edd9fe5d655261c062b48
WandiaG <wandiagithinji@gmail.com>
2024-01-13 01:54:32 -0800

    updating

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Script that prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+"""A script that prints the State passed as argument
+from database hbtn_0e_6_usa"""
+
+
+from sys import argv
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).filter_by(name=sys.argv[4]).first()
+
+    session = Session(engine)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
     if state:
         print("{}".format(state.id))
     else:
