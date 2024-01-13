8583f2793374efc7e64555c789c21cfc6be5bdff
WandiaG <wandiagithinji@gmail.com>
2024-01-13 01:32:24 -0800

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
- #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
-from sys import argv
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N from database hbtn_0e_0_usa"""
-import MySQLdb
-from sys import argv
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    cur.close()
-    db.close()
+        print(row)
